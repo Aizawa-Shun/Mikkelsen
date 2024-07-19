db6208582e9d08cadfc0c4dd3eb5afa12599ab3e
shun <shun.aizawa2001@gmail.com>
2024-07-19 14:01:50 +0900

    fixed simulation2d mode

--- utils/xz_sim.py
+++ utils/xz_sim.py
@@ -3,9 +3,9 @@ import pybullet as p
 def constrain_to_xz_plane(body_id):
     # In each simulation step, constrain the y position and velocity
     pos, orn = p.getBasePositionAndOrientation(body_id)
-    pos[1] = 0
-    p.resetBasePositionAndOrientation(body_id, pos, orn)
+    new_pos = [pos[0], 0, pos[2]]  # Lock y position to 0
+    p.resetBasePositionAndOrientation(body_id, new_pos, orn)
     
     linear_velocity, angular_velocity = p.getBaseVelocity(body_id)
-    linear_velocity[1] = 0
-    p.resetBaseVelocity(body_id, linearVelocity=linear_velocity, angularVelocity=angular_velocity)
+    new_linear_velocity = [linear_velocity[0], 0, linear_velocity[2]]  # Lock y velocity to 0
+    p.resetBaseVelocity(body_id, linearVelocity=new_linear_velocity, angularVelocity=angular_velocity)
