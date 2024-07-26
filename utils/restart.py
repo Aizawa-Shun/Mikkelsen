be6f8ba4cc41a04f0848089fff94571498777997
shun <shun.aizawa2001@gmail.com>
2024-07-26 19:23:40 +0900

    bat and sh files changes directory

--- utils/restart.py
+++ utils/restart.py
@@ -10,7 +10,7 @@ def restart_program():
 
     if os.name == 'nt':  # Windows
         # Running the bat file
-        subprocess.Popen([fr"{os.getcwd()}\\restart.bat"])
+        subprocess.Popen([fr"{os.getcwd()}\\utils\\restart.bat"])
     else:  # Linux or other UNIX-like systems
-        script_path = os.path.abspath(os.path.join(os.getcwd(), 'restart.sh'))
+        script_path = os.path.abspath(os.path.join(os.getcwd(), 'utils/restart.sh'))
         subprocess.Popen([script_path])
